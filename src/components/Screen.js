5aa1174e5b102366ddf56173aec92245bf194425
kevin Leonard <kevinleonard.info@gmail.com>
2019-11-29 15:57:19 +0100

    fix display

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -5,10 +5,10 @@ class Screen extends Component {
     return(
       <div className='display'>
         <ul className="list-stack">
+          <li className="actuelle">{this.props.current_value}</li>
           {this.props.historic.map((value, index) => {
             return <li key={index} className="historic">{value}</li>
           })}
-          <li className="actuelle">{this.props.current_value}</li>
         </ul>
       </div>
     )
